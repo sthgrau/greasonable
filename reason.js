dc35477fb31499a8b0ac260cc84c712dc836317c
sthg <sthg@hotmail.com>
2015-02-28 20:42:04 -0500

    changed version number

--- reason.js
+++ reason.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Full Reason
 // @namespace    http://github.com/sthgrau/greasonable
-// @version      0.5
+// @version      0.6
 // @description  does something useful
 // @author       Me
 // @match        http://reason.com/*
