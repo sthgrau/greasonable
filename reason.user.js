0f8503f74a5145c044ac07587711ab7d2a60136f
sthg <sthg@hotmail.com>
2015-03-01 18:16:55 -0500

    youtu.be support, removed iframe disable as it affected content videos as well as advertising videos

--- reason.user.js
+++ reason.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Full Reason
 // @namespace    http://github.com/sthgrau/greasonable
-// @version      0.8.1
+// @version      0.8.2
 // @description  does something useful
 // @author       Me
 // @match        http://reason.com/*
