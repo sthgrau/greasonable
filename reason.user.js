1140836f6c095bf71ec255da7df72fe633edacb9
sthg <sthg@hotmail.com>
2015-03-03 00:47:36 -0500

    testing set branch in name

--- reason.user.js
+++ reason.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Full Reason dev
 // @namespace    http://github.com/sthgrau/greasonable
-// @version      0.8.8
+// @version      0.8.8.1
 // @description  does something useful
 // @author       Me
 // @match        http://reason.com/*
