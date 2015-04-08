b0558622ca040c1840544ed97c1aba1afae6deb7
sthg <sthg@hotmail.com>
2015-04-08 10:27:35 -0400

    ff bugfix + version

--- reason.user.js
+++ reason.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Full Reason
 // @namespace    http://github.com/sthgrau/greasonable
-// @version      0.9.4.6.4
+// @version      0.9.4.6.5
 // @description  does something useful
 // @author       Me
 // @match        http://reason.com/*
